1d071cfdda95e89a4d8e8550df8082528fb39125
SanjidakramN <sanjidakram2@gmail.com>
2025-05-05 14:52:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
+}
+provider "aws" {
+  region = "us-east-1"
+  
+}
+resource "aws_instance" "registry-instance" {
+  ami = var.my-ami
+  instance_type = var.my-instance-type
+  tags = {
+    Name = "registry-instance"
+  }
