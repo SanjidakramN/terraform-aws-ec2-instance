5b8a6bd8c3219c19a3287810eb6ab1b4876d358e
SanjidakramN <sanjidakram2@gmail.com>
2025-05-05 15:57:53 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
-variable "my-ami" {
+variable "my_ami" {
   type = string
   default = "ami-0f88e80871fd81e91"
 }
-variable "my-instance-type" {
+variable "my_instance_type" {
   type = string
   default = "t2.micro"
 }
